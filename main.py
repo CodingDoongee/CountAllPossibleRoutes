d65b1923f5e69811cde6f097f1a177483b66f2f0
ohtaedong@kaist.ac.kr <Deeplearn!23>
2020-12-03 00:25:53 +0900

    Easy Code

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-Counter = 0
+from functools import lru_cache
 
 
 class Solution(object):
     def countRoutes(self, locations, start, finish, fuel):
-        self.FindChildren(locations, start, finish, fuel)
-        return Counter % (10**9+7)
+        @lru_cache(None)
+        def FindChildren(start, finish, fuel):
+            ans = 0
+            if start == finish:
+                ans += 1
+            for i in range(len(locations)):
+                if i != start:
+                    if fuel >= abs(locations[i] - locations[start]):
+                        ans += FindChildren(i, finish, fuel - abs(locations[i] - locations[start]))
+            return ans
+        return FindChildren(start=start, finish=finish, fuel=fuel) % (10 ** 9 + 7)
 
-    def FindChildren(self, locations, start, finish, fuel):
-        global Counter
-        for i in range(len(locations)):
-            if i == start:
-                continue
-            else:
-                if fuel >= abs(locations[i]-locations[start]):
-                    self.FindChildren(locations, i, finish, fuel - abs(locations[i]-locations[start]))
-                    if i == finish:
-                        Counter = Counter + 1
-
-    def __call__(self, *args, **kwargs):
-        print(Counter)
 
 a = Solution()
-a.countRoutes([4,3,1], 1, 0, 6)
-a()
+print(a.countRoutes([4, 3, 1], 1, 0, 6))
